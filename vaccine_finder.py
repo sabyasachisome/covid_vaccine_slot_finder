10c9d5baeaff78d6ddcf4ae1059f540fa65fb1cf
sabyasachisome <sabyasachisome123@gmail.com>
2021-07-27 00:40:54 +0530

    completing the slot finder app

--- vaccine_finder.py
+++ vaccine_finder.py
@@ -1,17 +1,15 @@
 import requests
 from object_modifier import ObjectModifier
 import os
+import json
 
 class LocationAssigner:
     def __init__(self, *args) -> None:
-        config_obj= args[0]
+        self.config_obj= args[0]
         self.district_url_param= {}
-        # print(config_obj.items('location_details'))
-        for tuple_idx in range(len(config_obj.items('location_details'))):
+        for tuple_idx in range(len(self.config_obj.items('location_details'))):
             # setattr(self, config_obj.items('location_details')[tuple_idx][0], config_obj.items('location_details')[tuple_idx][1])
-            self.district_url_param[config_obj.items('location_details')[tuple_idx][0]]= config_obj.items('location_details')[tuple_idx][1]
-        # print(self.state_code, self.district_code)
-        print(self.district_url_param)
+            self.district_url_param[self.config_obj.items('location_details')[tuple_idx][0]]= self.config_obj.items('location_details')[tuple_idx][1]
 
 
 class VaccineGenerator(LocationAssigner, ObjectModifier):
@@ -19,13 +17,25 @@ class VaccineGenerator(LocationAssigner, ObjectModifier):
         super(VaccineGenerator, self).__init__(*args)
     
     def check_availablity(self):
-        print(self.request_url)
-        requests.get(self.request_url)
+        resp= requests.get(self.config_obj.get('url_details','request_url'), params=self.district_url_param)
+        resp_dict= json.loads(resp.text)
+        # print(resp_dict)
+        for resp_dict_key, resp_dict_val in resp_dict.items():
+            pass
+        for idx in range(len(resp_dict_val)):
+            # print(resp_dict_val[idx])
+            if int(resp_dict_val[idx].get('available_capacity'))>0 and resp_dict_val[idx].get('min_age_limit')< int(self.config_obj.get('age_details','age')):
+                # print('yes')
+                # print(resp_dict_val[idx])
+                print(resp_dict_val[idx])
+                print(resp_dict_val[idx].get('available_capacity'))
+                # print(resp_dict_val[idx].get('min_age_limit'))
+
+
 
 if __name__ == '__main__':
     config_file_abs_path= os.path.abspath(os.path.join(os.path.dirname(__file__),'config_details.ini'))
-    # print(config_file_abs_path)
     obj_modify= ObjectModifier(config_file_abs_path)
     obj_modify.get_config()
     vaccine_generator= VaccineGenerator(obj_modify.config)
-    # vaccine_generator.check_availablity()
+    vaccine_generator.check_availablity()
